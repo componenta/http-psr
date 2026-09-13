4314b12a28ab9d03bacb777065d6a97a65bfc49c
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2026-09-14 01:19:27 +0200

    Release v1.1.0
    Publish accumulated local fixes and align dependency floors for the coordinated Componenta release.

--- tests/Psr7IntegrationConfigProviderTest.php
+++ tests/Psr7IntegrationConfigProviderTest.php
@@ -2,95 +2,50 @@
 
 declare(strict_types=1);
 
+use Componenta\Config\ConfigFactory;
 use Componenta\Config\ConfigKey;
-use Componenta\Http\Psr7\Diactoros\ConfigProvider as DiactorosConfigProvider;
-use Componenta\Http\Psr7\Guzzle\ConfigProvider as GuzzleConfigProvider;
-use Componenta\Http\Psr7\Nyholm\ConfigProvider as NyholmConfigProvider;
-use Componenta\Http\Psr7\Slim\ConfigProvider as SlimConfigProvider;
-use Psr\Http\Message\RequestFactoryInterface;
-use Psr\Http\Message\ResponseFactoryInterface;
+use Componenta\Config\Environment;
+use Componenta\DI\ContainerFactory;
+use Componenta\Http\ConfigProvider;
+use Nyholm\Psr7\Factory\Psr17Factory;
+use Nyholm\Psr7Server\ServerRequestCreatorInterface;
 use Psr\Http\Message\ServerRequestFactoryInterface;
 use Psr\Http\Message\StreamFactoryInterface;
 use Psr\Http\Message\UploadedFileFactoryInterface;
 use Psr\Http\Message\UriFactoryInterface;
 
-it('registers concrete psr17 aliases only in implementation packages', function (
-    string $provider,
-    array $aliases,
-    array $factories,
-    array $invokables,
-): void {
-    $config = (new $provider())();
-    $dependencies = $config[ConfigKey::DEPENDENCIES];
+it('creates incoming requests through the configured PSR factories', function (): void {
+    $factory = new Psr17Factory();
+    $composition = (new ConfigFactory())->create(new Environment([]),
+        new ConfigProvider(),
+        static fn (): array => [
+            ConfigKey::DEPENDENCIES => [
+                ConfigKey::SERVICES => [
+                    ServerRequestFactoryInterface::class => $factory,
+                    UriFactoryInterface::class => $factory,
+                    StreamFactoryInterface::class => $factory,
+                    UploadedFileFactoryInterface::class => $factory,
+                ],
+            ],
+        ],
+    );
+    $container = (new ContainerFactory())->create($composition->config, $composition->dependencies);
+    $creator = $container->get(ServerRequestCreatorInterface::class);
 
-    expect($dependencies[ConfigKey::ALIASES])->toMatchArray($aliases)
-        ->and(array_keys($dependencies[ConfigKey::FACTORIES] ?? []))->toBe($factories)
-        ->and($dependencies[ConfigKey::INVOKABLES] ?? [])->toBe($invokables);
-})->with([
-    'nyholm' => [
-        NyholmConfigProvider::class,
-        [
-            RequestFactoryInterface::class => 'Nyholm\\Psr7\\Factory\\Psr17Factory',
-            ResponseFactoryInterface::class => 'Nyholm\\Psr7\\Factory\\Psr17Factory',
-            ServerRequestFactoryInterface::class => 'Nyholm\\Psr7\\Factory\\Psr17Factory',
-            StreamFactoryInterface::class => 'Nyholm\\Psr7\\Factory\\Psr17Factory',
-            UploadedFileFactoryInterface::class => 'Nyholm\\Psr7\\Factory\\Psr17Factory',
-            UriFactoryInterface::class => 'Nyholm\\Psr7\\Factory\\Psr17Factory',
-        ],
-        ['Nyholm\\Psr7\\Factory\\Psr17Factory'],
-        [],
-    ],
-    'diactoros' => [
-        DiactorosConfigProvider::class,
-        [
-            RequestFactoryInterface::class => 'Laminas\\Diactoros\\RequestFactory',
-            ResponseFactoryInterface::class => 'Laminas\\Diactoros\\ResponseFactory',
-            ServerRequestFactoryInterface::class => 'Laminas\\Diactoros\\ServerRequestFactory',
-            StreamFactoryInterface::class => 'Laminas\\Diactoros\\StreamFactory',
-            UploadedFileFactoryInterface::class => 'Laminas\\Diactoros\\UploadedFileFactory',
-            UriFactoryInterface::class => 'Laminas\\Diactoros\\UriFactory',
-        ],
-        [],
-        [
-            'Laminas\\Diactoros\\RequestFactory',
-            'Laminas\\Diactoros\\ResponseFactory',
-            'Laminas\\Diactoros\\ServerRequestFactory',
-            'Laminas\\Diactoros\\StreamFactory',
-            'Laminas\\Diactoros\\UploadedFileFactory',
-            'Laminas\\Diactoros\\UriFactory',
-        ],
-    ],
-    'guzzle' => [
-        GuzzleConfigProvider::class,
-        [
-            RequestFactoryInterface::class => 'GuzzleHttp\\Psr7\\HttpFactory',
-            ResponseFactoryInterface::class => 'GuzzleHttp\\Psr7\\HttpFactory',
-            ServerRequestFactoryInterface::class => 'GuzzleHttp\\Psr7\\HttpFactory',
-            StreamFactoryInterface::class => 'GuzzleHttp\\Psr7\\HttpFactory',
-            UploadedFileFactoryInterface::class => 'GuzzleHttp\\Psr7\\HttpFactory',
-            UriFactoryInterface::class => 'GuzzleHttp\\Psr7\\HttpFactory',
-        ],
-        ['GuzzleHttp\\Psr7\\HttpFactory'],
-        [],
-    ],
-    'slim' => [
-        SlimConfigProvider::class,
-        [
-            RequestFactoryInterface::class => 'Slim\\Psr7\\Factory\\RequestFactory',
-            ResponseFactoryInterface::class => 'Slim\\Psr7\\Factory\\ResponseFactory',
-            ServerRequestFactoryInterface::class => 'Slim\\Psr7\\Factory\\ServerRequestFactory',
-            StreamFactoryInterface::class => 'Slim\\Psr7\\Factory\\StreamFactory',
-            UploadedFileFactoryInterface::class => 'Slim\\Psr7\\Factory\\UploadedFileFactory',
-            UriFactoryInterface::class => 'Slim\\Psr7\\Factory\\UriFactory',
-        ],
-        [],
-        [
-            'Slim\\Psr7\\Factory\\RequestFactory',
-            'Slim\\Psr7\\Factory\\ResponseFactory',
-            'Slim\\Psr7\\Factory\\ServerRequestFactory',
-            'Slim\\Psr7\\Factory\\StreamFactory',
-            'Slim\\Psr7\\Factory\\UploadedFileFactory',
-            'Slim\\Psr7\\Factory\\UriFactory',
-        ],
-    ],
-]);
+    $request = $creator->fromArrays(
+        server: ['REQUEST_METHOD' => 'POST', 'REQUEST_URI' => '/orders?id=1', 'QUERY_STRING' => 'id=1', 'SERVER_NAME' => 'example.test', 'SERVER_PORT' => '80'],
+        headers: ['Content-Type' => 'application/x-www-form-urlencoded'],
+        cookie: ['session' => 'test-session'],
+        get: ['id' => '1'],
+        post: ['name' => 'Ada'],
+        body: 'name=Ada',
+    );
+
+    expect($request->getMethod())->toBe('POST')
+        ->and((string) $request->getUri())->toBe('http://example.test/orders?id=1')
+        ->and($request->getCookieParams())->toBe(['session' => 'test-session'])
+        ->and($request->getQueryParams())->toBe(['id' => '1'])
+        ->and($request->getParsedBody())->toBe(['name' => 'Ada'])
+        ->and($request->getHeaderLine('Content-Type'))->toBe('application/x-www-form-urlencoded')
+        ->and((string) $request->getBody())->toBe('name=Ada');
+});
